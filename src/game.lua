f9cfc839ecced1f63567438baef70c1645fa06db
= <tiago.panizio@hotmail.com>
2024-06-10 16:58:37 -0300

    auto editor prettify

--- src/game.lua
+++ src/game.lua
@@ -1,42 +1,42 @@
 local Game = {}
 
 Game.levels = {
-    require "levels.menu",
-    require "levels.level1",
-    require "levels.level2",
-    require "levels.level3"
+  require "levels.menu",
+  require "levels.level1",
+  require "levels.level2",
+  require "levels.level3"
 }
 
 Game.currentLevel = 1
 
 function Game.load()
-    Game.levels[Game.currentLevel].load()
+  Game.levels[Game.currentLevel].load()
 end
 
 function Game.update(dt)
-    Game.levels[Game.currentLevel].update(dt)
+  Game.levels[Game.currentLevel].update(dt)
 end
 
 function Game.draw()
-    Game.levels[Game.currentLevel].draw()
+  Game.levels[Game.currentLevel].draw()
 end
 
 function Game.keypressed(key)
-    if key == "n" then
-        Game.currentLevel = Game.currentLevel % #Game.levels + 1
-        Game.levels[Game.currentLevel].load()
-    end
-    Game.levels[Game.currentLevel].keypressed(key)
+  if key == "n" then
+    Game.currentLevel = Game.currentLevel % #Game.levels + 1
+    Game.levels[Game.currentLevel].load()
+  end
+  Game.levels[Game.currentLevel].keypressed(key)
 end
 
 function Game.mousepressed(x, y, button, istouch, presses)
-  if(Game.levels[Game.currentLevel].mousepressed) then
+  if (Game.levels[Game.currentLevel].mousepressed) then
     Game.levels[Game.currentLevel].mousepressed(x, y, button, istouch, presses)
   end
 end
 
-function Game.mousereleased( x, y, button, istouch, presses )
-  if(Game.levels[Game.currentLevel].mousereleased) then
+function Game.mousereleased(x, y, button, istouch, presses)
+  if (Game.levels[Game.currentLevel].mousereleased) then
     Game.levels[Game.currentLevel].mousereleased(x, y, button, istouch, presses)
   end
 end
