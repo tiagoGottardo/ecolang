ac55e3e252d4244036ba7ec84654b1204c147102
= <tiago.panizio@hotmail.com>
2024-06-10 16:57:09 -0300

    apply shape features to lvl1

--- levels/level1.lua
+++ levels/level1.lua
@@ -1,20 +1,32 @@
+local shape = require "src.shape"
 local Level1 = {}
+local container = {}
+local header = {}
 
 function Level1.load()
-    -- Carregar recursos específicos
+  container = shape:newRect({ width = 80, height = 80, x = 50, y = 50 })
+  container:setColor({ 148, 221, 114, 42 })
+  header = shape:newRect({ width = 80, height = 15 })
+  header:setColor({ 148, 221, 204, 69 })
+  header:setPosition({ 50, 7.5 }, container)
 end
 
 function Level1.update(dt)
-    -- Atualizar lógica
+
 end
 
 function Level1.draw()
-    love.graphics.print("Fase1", 400, 300)
-    -- Desenhar elementos
+  love.graphics.setBackgroundColor(1, 1, 1)
+  container:draw(love)
+  header:draw(love)
+
+  love.graphics.print("Fase1", 400, 300)
 end
 
 function Level1.keypressed(key)
-    -- Lidar com teclas pressionadas
+  if key == "q" then
+    love.event.quit()
+  end
 end
 
 return Level1
