c53da97ff6edc3c522f2162082b0bf0d1fa22cdd
= <tiago.panizio@hotmail.com>
2024-06-12 16:32:26 -0300

    integrate content component into object

--- components/object.lua
+++ components/object.lua
@@ -2,6 +2,8 @@ local Rectangle = require "components.rectangle"
 local Circle = require "components.circle"
 local Square = require "components.square"
 
+local Image = require "components.image"
+local Text = require "components.text"
 local Color = require "components.color"
 local Position = require "components.position"
 
@@ -27,17 +29,35 @@ local function pickShape(shapeTable)
   return shapes[shapeTable.kind](shapeTable)
 end
 
+local function pickContent(contentTable)
+  contentTable = contentTable or {}
+  contentTable.kind = contentTable.kind or 'text'
+
+  local contents = {
+    ['image'] = function(t)
+      return Image:new(t)
+    end,
+    ['text'] = function(t)
+      return Text:new(t)
+    end
+  }
+
+  return contents[contentTable.kind](contentTable)
+end
+
 function Object:new(objectTable)
   objectTable = objectTable or {}
   local instance = setmetatable({}, Object)
   instance.color = Color:new(objectTable.color)
   instance.position = Position:new(objectTable.position)
   instance.shape = pickShape(objectTable.shape)
+  instance.content = pickContent(objectTable.content)
   return instance
 end
 
 function Object:set(objectTable)
   objectTable = objectTable or {}
+  self.content:set(objectTable.content)
   self.color:set(objectTable.color)
   self.position:set(objectTable.position)
   self.shape:set(objectTable.shape)
@@ -46,6 +66,11 @@ end
 function Object:draw()
   local r, g, b, a = self.color:format()
   self.shape:draw(r, g, b, a, self.position:get())
+  if getmetatable(self.shape) == Rectangle and self.content.label ~= "" then
+    local size = self.shape:get()
+    local position = self.position:get()
+    self.content:draw(position[1] + size.width / 2, position[2] + size.height / 2)
+  end
 end
 
 function Object:debug()
@@ -53,6 +78,7 @@ function Object:debug()
   self.shape:debug()
   self.color:debug()
   self.position:debug()
+  self.content:debug()
   print(")")
 end
 
