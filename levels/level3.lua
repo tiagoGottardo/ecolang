1262803a64d5a118f92da27b1ff826374af7eb90
Gustavo Santos <claudineygrsantos@gmail.com>
2024-06-23 16:26:39 -0300

    Feita a base funcional da fase 3

--- levels/level3.lua
+++ levels/level3.lua
@@ -1,20 +1,188 @@
 local Level3 = {}
+require("utils.colors")
+local utils = require 'utils'
+local utf8 = require 'utf8'
+local Object = require 'components.object'
+local Timer = require 'src.timer'
+local Button = require "components.button"
+local Text = require "components.text"
+local Image = require "components.image"
+
+local cursor = {}
+local Cursor = require 'src.cursor'
+
+local container = {}
+local centralContainer = require 'levels.components.centralContainer'
+
+local header = {}
+local upHeader = require "levels.components.upHeader"
+
+local headerLabel = {}
+local componentHeaderLabel = require "levels.components.componentHeaderLabel"
+
+local soundHeader = {}
+local componentSoundHeader = require "levels.components.componentSoundHeader"
+
+local logo = {}
+
+local helpButton = {}
+local componentHelpButton = require "levels.components.componentHelpButton"
+
+local animalSound
+local animalImage
+
+local successModal
+local componentSucces = require "levels.components.componentSucces"
+
+local failedModal
+local componentFailed = require "levels.components.componentFailed"
+
+local isTimeOverModal
+local componentTimeOver = require "levels.components.componentTimeOver"
+
+local evenTriggered = false
 
 function Level3.load()
-    -- Carregar recursos específicos
+  evenTriggered = false
+  animalSound = love.audio.newSource("assets/sounds/" .. Game.animal .. ".mp3", "static")
+
+  -- letterGoal = Object:new {
+  --   color = { a = 0 },
+  --   position = { WINDOW_WIDTH * 2 / 6, 97 },
+  container = centralContainer:new()
+
+  header = upHeader:new()
+
+  headerLabel = componentHeaderLabel:new(nil)
+
+  animalImage = Object:new {
+    position = { WINDOW_WIDTH * 4 / 5, 97 },
+    shape = {
+      width = 88,
+      height = 88,
+      radius = 20,
+    },
+    content = {
+      kind = 'image',
+      width = 70,
+      height = 70,
+      name = "Animals/" .. Game.animal .. ".png"
+    }
+  }
+
+  soundHeader = componentSoundHeader:new()
+
+  logo = Image:new({ name = "logo.png", width = 325 * 0.4, height = 152 * 0.4, })
+
+  helpButton = componentHelpButton:new()
+
+  isTimeOverModal = componentTimeOver:new()
+
+  successModal = componentSucces:new("VOLTAR AO MENU")
+
+  failedModal = componentFailed:new()
+
+  cursor = Cursor:new {
+    botoes = { soundHeader, helpButton }
+  }
+end
+
+local function verifyCorrectAnswer(answer)
+  answer = answer or ""
+  if Game.animal == answer then
+    successModal.hidden = false
+    cursor:set { botoes = { successModal.button } }
+  else
+    --failedModal.hidden = false
+    --cursor:set { botoes = { failedModal.button } }
+  end
+end
+
+function Level3.mousepressed(x, y, button)
+  -- print(utils.string:tostring{x, y})
+  if (failedModal.hidden and successModal.hidden and isTimeOverModal.hidden) then
+    soundHeader:onClick(x, y, button, (function() animalSound:play() end))
+    helpButton:onClick(x, y, button, (function()
+      Game.currentLevel = 6
+      Game.load()
+    end))
+  end
+  if not failedModal.hidden then
+    failedModal.button:onClick(x, y, button, (function()
+      Game.load()
+    end))
+  end
+  if not successModal.hidden then
+    successModal.button:onClick(x, y, button, (function()
+      Game.currentLevel = 1
+      Game.load()
+    end))
+  end
+  if not isTimeOverModal.hidden then
+    isTimeOverModal.button:onClick(x, y, button, (function()
+      Game.currentLevel = 1
+      Game.load()
+    end))
+  end
+end
+
+function Level3.mousemoved(x, y, dx, dy, istouch)
+  cursor:update(x, y)
 end
 
 function Level3.update(dt)
-    -- Atualizar lógica
+  Game.timer:update(dt)
+  if Game.timer:isTimeOver() and not evenTriggered then
+    isTimeOverModal.hidden = false
+    cursor:set { botoes = { isTimeOverModal.button } }
+    evenTriggered = true
+  end
 end
 
 function Level3.draw()
-    love.graphics.print("FASE3", 400, 300)
-    -- Desenhar elementos
+  container:draw()
+  header:draw()
+  headerLabel:draw()
+
+  local myFont       = love.graphics.newFont("assets/Sniglet/Sniglet-Regular.ttf", 65)
+  local previousFont = love.graphics.getFont()
+  love.graphics.setFont(myFont)
+
+
+  local highlightColor = { 0.8, 0, 0, 1 }
+  local regularColor = { 0.027, 0.545, 0.141, 1 }
+  local coloredText = { regularColor, Game.animal }
+  local textWidth = 0
+  local textHeight = myFont:getHeight()
+  for i, v in ipairs(coloredText) do
+    if i % 2 == 0 then
+      textWidth = textWidth + myFont:getWidth(v)
+    end
+  end
+  love.graphics.print(coloredText, myFont, WINDOW_WIDTH / 2 - textWidth / 2, 97 - textHeight / 2)
+
+  soundHeader:draw()
+  animalImage:draw()
+  Game.timer:draw(900, 20)
+  logo:draw(325 * 0.2, 152 * 0.2)
+  helpButton:draw()
+  helpButton:setBorder()
+  if not failedModal.hidden then
+    failedModal:draw()
+  end
+  if not isTimeOverModal.hidden then
+    isTimeOverModal:draw()
+  end
+  if not successModal.hidden then
+    successModal:draw()
+  end
 end
 
-function Level3.keypressed(key)
-    -- Lidar com teclas pressionadas
+function Level3.textinput(text)
+  if text then
+    print(utils.string:tostring(text))
+    verifyCorrectAnswer(text)
+  end
 end
 
 return Level3
